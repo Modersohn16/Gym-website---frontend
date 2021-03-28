099a68182fc70f9cf86845ad3c4f766e0d8bdbbe
Modersohn16 <40393958+Modersohn16@users.noreply.github.com>
2021-03-28 23:59:20 +0200

    Modificating GymList file to adjust to Booking

--- gym-website3/src/components/BookingTrClass.js
+++ gym-website3/src/components/BookingTrClass.js
@@ -3,8 +3,64 @@ import "BookingTrClass.css";
 
 const Booking = () => 
 <>
-<div class="booking">
+function GymList() {
+   const [Gyms, setGyms] = useState(null);
+   const GetGymUrl = "https://localhost:44364/api/User/GetGym";
+   const Init = {
+       method: "GET",
+       headers: {"Content-type": "application/json;charset=UTF-8",
+       'Accept': 'application/json',
+       "Access-Control-Allow-Origin": "*"}  
+   };
+    useEffect(() => {
+       fetch(GetGymUrl, Init).then(response => response.json())
+       .then(data => setGyms(data));
+   }, [GetGymUrl]) 
+  
+  
+if(Gyms != null)
+{
+       return (
+           <>
+           <div><h1>OUR GYMS</h1></div>
+           <div title="summary">
+    <table className="Table" width="100%" border="1">
+    <tbody>
+    <tr>
+      <td>NAME</td>
+      <td>ADDRESS</td>
+      <td>MAX PEOPLE</td>
+      <td>OPERATIONAL HOURS</td>
+      <td>EMAIL</td>
+      <td>PHONE NUMBER</td>
+    </tr>
+{ 
+Gyms.map(gym => 
+            <tr>
+            <td><span>{gym.name}</span></td>
+            <td><span>{gym.streetAdress}<br/></span>
+            <span>{gym.postalCode}<br/></span>
+            <span>{gym.city}</span></td>
+            <td><span>{gym.maxPeople}</span></td>
+            <td><span>{gym.operationalHours}</span></td>
+            <td><span>{gym.email}</span></td>
+            <td><span>{gym.phoneNumber}</span></td>
+            </tr>)
+} 
 
+
+        
+    
+    </tbody>
+    </table>
+    </div>
+           </>
+           );
+   }
+
+   return <p></p>;
+
+}
 </div>
 </>
 
